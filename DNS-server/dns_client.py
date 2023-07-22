85f13328e2e6a7adcbe640464dd27e975678ce5f
Lyndon Purcell <lyndonpurcell23@gmail.com>
2023-07-22 16:50:38 +1000

    server creating TID and flag components of response message

--- DNS-server/dns_client.py
+++ DNS-server/dns_client.py
@@ -1,11 +1,31 @@
 from socket import socket, AF_INET, SOCK_DGRAM
+from secrets import randbits
 
 HOST = "127.0.0.1" # server's IP address
-PORT = 8080 # port being used by the server
+PORT = 8080 # port being used by the server -- make standard DNS port?
+QUIT_COMMAND = ":q!"
 
-# creates a socket that uses UDP
+def build_query(url):
+    transaction_id = format(randbits(16), '016b')
+    # flags
+    qr = 0
+    op_code = format(0, '04b')
+    recursion_desired = 0
+
+# creates a socket that uses UDP 
 with socket(AF_INET, SOCK_DGRAM) as s:
     s.connect((HOST, PORT)) # connects to the servers socket
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-    print(f"Received {data}")
+
+    more_queries = True
+
+    while more_queries:
+        hostname = input("Hostname or alias: ")
+
+        query_message = b""
+
+        s.send(query_message)
+        response_message = s.recv(1024)
+        print(f"Received {response_message}")
+
+        answer = input(f"Type '{QUIT_COMMAND}' to quit")
+        more_queries = answer == QUIT_COMMAND
--- DNS-server/dns_server.py
+++ DNS-server/dns_server.py
@@ -3,16 +3,53 @@ from socket import socket, AF_INET, SOCK_DGRAM
 HOST = "127.0.0.1"
 PORT = 8080
 
+def get_transaction_id(id_bytes):
+    tid = ""
+    for byte in id_bytes[:2]: # first two byes are the TID
+        tid += hex(byte)[2:] # strip leading hexcode
+    return tid
+
+def get_flags(flag_bytes):
+    byte1 = flag_bytes[:1]
+    byte2 = flag_bytes[1:]
+
+    # BYTE 1
+    QR = "1" 
+    OPCODE = ""
+    for bit_shift in range(6, 3, -1):
+        OPCODE += "1" if ord(byte1) & (1 << bit_shift) else "0"
+    AA = "0"
+    TC = "0"
+    RD = "1" if ord(byte1) & 1 else "0"
+    r_byte1 = int(QR + OPCODE + AA + TC + RD, 2).to_bytes(1, 'big')
+
+    # BYTE 2
+    RA = "0"
+    Z = "0"
+    AD = "0"
+    CD = "0"
+    RCODE = "0000"
+    r_byte2 = int(RA + Z + AD + CD + RCODE, 2).to_bytes(1, 'big')
+    print(r_byte1 + r_byte2)
+    return r_byte1 + r_byte2
+
+def build_response(query):
+    transaction_id = get_transaction_id(query[:2])
+    flags = get_flags(query[2:4])
+    # # flags
+    # response_code = format(0, '04b') # 0 = no error
+    return
+
 # creates a socket that uses UDP
 with socket(AF_INET, SOCK_DGRAM) as s:
     s.bind((HOST, PORT)) # associates the socket with a network interface and port
     print(f"Server running on {HOST}:{PORT}...")
 
     while True:
-        data, addr = s.recvfrom(1024)
-        if not data:
+        query, addr = s.recvfrom(1024)
+        if not query:
             break
-        print(f"Recieved {data} from {addr}")
+        response = build_response(query)
         s.sendto(b"Hello back!", addr)
 
 
